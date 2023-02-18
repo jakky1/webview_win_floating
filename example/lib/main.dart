9bd6f361d981c4566a95487302450e8c51888379
jakky1 <jakky1@gmail.com>
2023-02-18 20:03:23 +0800

    modify example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,17 +17,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final controller = WebViewController();
+  WebViewController? controller;
   final srollController = ScrollController();
+  bool isShown = true;
 
   @override
   void initState() {
     super.initState();
-    controller.setJavaScriptMode(JavaScriptMode.unrestricted);
-    controller.setBackgroundColor(Colors.cyanAccent);
-    controller.setNavigationDelegate(NavigationDelegate(
+    createController();
+  }
+
+  void createController() {
+    controller = WebViewController();
+    controller!.setJavaScriptMode(JavaScriptMode.unrestricted);
+    controller!.setBackgroundColor(Colors.cyanAccent);
+    controller!.setNavigationDelegate(NavigationDelegate(
       onNavigationRequest: (request) {
-        if (request.url.startsWith("https://www.google.com")) {
+        if (request.url.startsWith("https://www.youtube.com")) {
           return NavigationDecision.navigate;
         } else {
           log("prevent user navigate out of google website!");
@@ -39,12 +45,50 @@ class _MyAppState extends State<MyApp> {
       onWebResourceError: (error) =>
           print("onWebResourceError: ${error.description}"),
     ));
-    controller.loadRequest(Uri.parse("https://www.google.com/"));
+    controller!.loadRequest(Uri.parse("https://www.youtube.com/"));
+
+    isShown = true;
+  }
+
+  void createOrDelete() {
+    if (controller == null) {
+      createController();
+    } else {
+      controller = null;
+    }
+    setState(() {});
+  }
+
+  void hideOrShow() {
+    isShown = !isShown;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget webview = WebViewWidget(controller: controller);
+    Widget webview = (controller != null && isShown)
+        ? WebViewWidget(controller: controller!)
+        : const SizedBox.shrink();
+
+    Widget buttons = Row(children: [
+      ElevatedButton(
+          onPressed: createOrDelete,
+          child:
+              controller != null ? const Text("Delete") : const Text("Create")),
+      if (controller != null)
+        ElevatedButton(
+            onPressed: hideOrShow,
+            child: isShown ? const Text("Hide") : const Text("Show")),
+    ]);
+
+    webview = SingleChildScrollView(
+      child: Column(children: [
+        buttons,
+        Container(height: 200, color: Colors.blue[700]),
+        Container(height: 500, color: Colors.red[700], child: webview),
+        Container(height: 1300, color: Colors.green[700]),
+      ]),
+    );
 
     return MaterialApp(
       home: Scaffold(
