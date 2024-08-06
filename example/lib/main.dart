d5fda73a0ab04e698d72d46c00094cf04c4329a7
jakky1 <jakky1@gmail.com>
2024-08-07 00:00:24 +0800

    add functions in example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final controller = WebViewController();
+  final urlController = TextEditingController();
 
   @override
   void initState() {
@@ -22,6 +23,7 @@ class _MyAppState extends State<MyApp> {
     controller.setJavaScriptMode(JavaScriptMode.unrestricted);
     controller.setBackgroundColor(Colors.cyanAccent);
     controller.setNavigationDelegate(NavigationDelegate(
+      /*
       onNavigationRequest: (request) {
         if (request.url.startsWith("https://www.youtube.com")) {
           return NavigationDecision.navigate;
@@ -30,22 +32,75 @@ class _MyAppState extends State<MyApp> {
           return NavigationDecision.prevent;
         }
       },
-      onPageStarted: (url) => print("onPageStarted: $url"),
+      */
+      onPageStarted: (url) {
+        urlController.text = url;
+        print("onPageStarted: $url");
+      },
       onPageFinished: (url) => print("onPageFinished: $url"),
       onWebResourceError: (error) =>
           print("onWebResourceError: ${error.description}"),
     ));
-    controller.loadRequest(Uri.parse("https://www.youtube.com/"));
+    controller.loadRequest(Uri.parse("https://www.google.com/"));
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Windows Webview example app'),
+    Widget urlBox = TextField(
+      controller: urlController,
+      onSubmitted: (url) {
+        url = url.trim();
+        if (!url.startsWith("http")) {
+          url = "https://$url";
+        }
+        controller.loadRequest(Uri.parse(url));
+      },
+    );
+    Widget buttonRow = Row(children: [
+      //MyCircleButton(icon: Icons.arrow_back, onTap: controller.goBack),
+      //MyCircleButton(icon: Icons.arrow_forward, onTap: controller.goForward),
+      MyCircleButton(
+          icon: Icons.arrow_back,
+          onTap: () {
+            controller.runJavaScript("history.back();");
+          }),
+      MyCircleButton(
+          icon: Icons.arrow_forward,
+          onTap: () {
+            controller.runJavaScript("history.forward();");
+          }),
+      MyCircleButton(icon: Icons.refresh, onTap: controller.reload),
+      Expanded(child: urlBox),
+    ]);
+
+    Widget body =
+        Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      buttonRow,
+      Expanded(child: WebViewWidget(controller: controller)),
+    ]);
+
+    return MaterialApp(home: Scaffold(body: body));
+  }
+}
+
+class MyCircleButton extends StatelessWidget {
+  final GestureTapCallback? onTap;
+  final IconData icon;
+  final double size;
+
+  const MyCircleButton(
+      {super.key, required this.onTap, required this.icon, this.size = 32});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Material(
+        color: Colors.blue, // Button color
+        child: InkWell(
+          splashColor: Colors.red, // Splash color
+          onTap: onTap,
+          child: SizedBox(width: size, height: size, child: Icon(icon)),
         ),
-        body: WebViewWidget(controller: controller),
       ),
     );
   }
